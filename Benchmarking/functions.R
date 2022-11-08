c4297aba2e6535531a2d98a161e0f54fd4a04d6d
Sebastian Dall <semoda@bio.aau.dk>
2022-11-08 15:46:53 +0100

    Made new figures in benchmarking

---
+++ Benchmarking/functions.R
@@ -0,0 +1,98 @@
+
+# theme
+articletheme <- theme_bw(base_size = 10) +
+    theme(
+        axis.text.x = element_text(angle = 90, face = "bold", vjust = 0.5, size = 10, hjust = 1),
+        axis.text.y = element_text(face = "bold", size = 10),
+        plot.margin = unit(c(0, 1, 0, 0), "cm"),
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 10),
+        strip.text.x = element_text(size = 7, face = "bold"),
+        strip.text.y = element_text(size = 7, face = "bold"),
+        legend.title = element_text(face = "bold"),
+        legend.text = element_text(face = "bold", size = 10)
+    )
+
+
+# Ordination function
+plotAmpliconOrdination <- function(df, ord_type = "CA", rel_ab_filter = 0.1) {
+    df %>%
+        amp_ordinate(
+            # sample_color_by = "Soil_type",
+            sample_shape_by = "Kit",
+            filter_species = rel_ab_filter,
+            type = ord_type,
+            transform = "hellinger",
+            distmeasure = "bray",
+            sample_colorframe = F,
+            species_plot = T
+        ) +
+        ggforce::geom_mark_ellipse(aes(fill = Soil_type, group = Soil_type), alpha = 0.5, color = "black") + # ggtitle("PCoA of microbial diversity", subtitle = bquote(atop(c("PERMANOVA", "Soil type:" ~R^2~ "= 0.54, p-value = 0.001", "Protocol:" ~R^2~ "= 0.02, p-value = 0.214")))) +
+        labs(
+            title = paste0(ord_type, " of microbial diversity"),
+            fill = "Sample type",
+            shape = "Extraction kit"
+        ) +
+        articletheme +
+        theme(
+            legend.position = "none",
+            axis.text.x = element_text(angle = 0)
+        )
+}
+
+addTableColor <- function(table, df, filter_col, column) {
+    NA_vector <- df %>%
+        filter(variable == filter_col, is.na(value))
+
+    new_tab <- table_cell_font(table, row = NA_vector$rownumber, column = column, face = "italic", color = "red")
+    return(new_tab)
+}
+
+# abundance_filter <- function(df, abundance_threshold = 0.1, verbose = F) {
+#     n_before <- ncol(df)
+
+#     df_filter <- df %>%
+#         rownames_to_column("id") %>%
+#         pivot_longer(-id, names_to = "clade_name", values_to = "relative_abundance") %>%
+#         group_by(id) %>%
+#         mutate(relative_abundance = relative_abundance / sum(relative_abundance)) %>%
+#         group_by(clade_name) %>%
+#         summarise(relative_abundance = max(relative_abundance)) %>%
+#         filter(relative_abundance >= abundance_threshold / 100)
+
+#     df_filtered <- df %>%
+#         select(df_filter$clade_name)
+
+#     n_after <- ncol(df_filtered)
+#     if (verbose) {
+#         cat(paste0(
+#             "Species before: ", n_before, "\n",
+#             "Species after: ", n_after, "\n",
+#             "Removed species: ", n_before - n_after, "\n"
+#         ))
+#     }
+
+#     return(df_filtered)
+# }
+
+
+
+# plotDiffRelabund <- function(df, filter_soil_type) {
+#     soiltypeTitle <- df %>%
+#         filter(Soil_type == filter_soil_type) %>%
+#         distinct(Soil_type)
+
+#     gg <- df %>%
+#         filter(Soil_type == filter_soil_type) %>%
+#         ggplot(aes(x = Fullscale, y = Downscaled, color = Bias)) +
+#         geom_point(size = 4, alpha = 0.5, position = position_jitter()) +
+#         scale_color_manual(values = c("grey", "red", "blue")) +
+#         geom_abline(size = 0.5) +
+#         articletheme +
+#         theme(
+#             legend.position = "bottom",
+#             axis.text.x = element_text(angle = 0)
+#         ) +
+#         labs(x = "Relative Abundance [%], 2 x 25 µL", y = "Relative Abundance [%], 2 x 5 µL", title = paste0("Amplicon Read Relative Abundance for ", soiltypeTitle$Soil_type[1]))
+
+#     return(gg)
+# }
