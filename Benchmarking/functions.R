2741a5160f6b9b02c9ddf819390c11eebc98221b
SebastianDall <semoda@bio.aau.dk>
2022-11-09 15:19:47 +0100

    Added zoom on plot benchmark

--- Benchmarking/functions.R
+++ Benchmarking/functions.R
@@ -5,7 +5,7 @@ articletheme <- theme_bw(base_size = 10) +
         axis.text.x = element_text(angle = 90, face = "bold", vjust = 0.5, size = 10, hjust = 1),
         axis.text.y = element_text(face = "bold", size = 10),
         plot.margin = unit(c(0, 1, 0, 0), "cm"),
-        plot.title = element_text(hjust = 0.5, face = "bold", size = 10),
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 12),
         strip.text.x = element_text(size = 7, face = "bold"),
         strip.text.y = element_text(size = 7, face = "bold"),
         legend.title = element_text(face = "bold"),
