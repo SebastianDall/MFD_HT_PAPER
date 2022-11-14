7e3d12ba200ebd7afe0f6b901082f55ac6dd1a35
Sebastian Dall <semoda@bio.aau.dk>
2022-11-14 10:24:23 +0100

    Added articletheme to a separat script.

--- Amplicon_scaledown/functions.R
+++ Amplicon_scaledown/functions.R
@@ -1,18 +1,3 @@
-
-# theme
-articletheme <- theme_bw(base_size = 10) +
-    theme(
-        axis.text.x = element_text(angle = 90, face = "bold", vjust = 0.5, size = 10, hjust = 1),
-        axis.text.y = element_text(face = "bold", size = 10),
-        plot.margin = unit(c(0, 1, 0, 0), "cm"),
-        plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
-        strip.text.x = element_text(size = 7, face = "bold"),
-        strip.text.y = element_text(size = 7, face = "bold"),
-        legend.title = element_text(face = "bold"),
-        legend.text = element_text(face = "bold", size = 10)
-    )
-
-
 # Ordination function
 plotAmpliconOrdination <- function(df, ord_type = "CA", rel_ab_filter = 0.1) {
     df %>%
--- Benchmarking/functions.R
+++ Benchmarking/functions.R
@@ -3,20 +3,6 @@ library(ggpubr)
 library(RColorBrewer)
 
 
-# theme
-articletheme <- theme_bw(base_size = 10) +
-    theme(
-        axis.text.x = element_text(angle = 90, face = "bold", vjust = 0.5, size = 10, hjust = 1),
-        axis.text.y = element_text(face = "bold", size = 10),
-        plot.margin = unit(c(0, 1, 0, 0), "cm"),
-        plot.title = element_text(hjust = 0.5, face = "bold", size = 12),
-        strip.text.x = element_text(size = 7, face = "bold"),
-        strip.text.y = element_text(size = 7, face = "bold"),
-        legend.title = element_text(face = "bold"),
-        legend.text = element_text(face = "bold", size = 10)
-    )
-
-
 # Ordination function
 plotAmpliconOrdination <- function(df, ord_type = "CA", rel_ab_filter = 0.1) {
     df %>%
