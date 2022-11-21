a3445f857e4ad6d425f739d5faf083aa2065f9a3
Sebastian Dall <semoda@bio.aau.dk>
2022-11-21 10:04:00 +0100

    Updated amplicon scale down figures

--- Amplicon_scaledown/functions.R
+++ Amplicon_scaledown/functions.R
@@ -1,26 +1,25 @@
 # Ordination function
-plotAmpliconOrdination <- function(df, ord_type = "CA", rel_ab_filter = 0.1) {
+plotAmpliconOrdinationSoil <- function(df, soil_filter, ord_type = "CA", rel_ab_filter = 0.1) {
     df %>%
+        amp_subset_samples(Soil_type == soil_filter) %>%
         amp_ordinate(
-            sample_color_by = "Soil_type",
-            sample_shape_by = "Protocol",
-            filter_species = rel_ab_filter,
+            filter_species = 0.1,
             type = ord_type,
+            constrain = "Protocol",
             transform = "hellinger",
             distmeasure = "bray",
-            sample_colorframe = F,
+            sample_colorframe = "Protocol",
+            # species_nlabels = 5,
+            # species_label_taxonomy = "Phylum",
             species_plot = T
         ) +
-        ggforce::geom_mark_ellipse(aes(fill = Soil_type, color = Soil_type)) +
-        # ggtitle("PCoA of microbial diversity", subtitle = bquote(atop(c("PERMANOVA", "Soil type:" ~R^2~ "= 0.54, p-value = 0.001", "Protocol:" ~R^2~ "= 0.02, p-value = 0.214")))) +
         labs(
-            title = paste0(ord_type, " of microbial diversity"),
-            fill = "Sample type"
+            title = paste0(soil_filter, ": ", ord_type, " of microbial diversity")
         ) +
         articletheme +
         theme(
             legend.position = "none",
-            axis.text.x = element_text(angle = 0)
+            axis.text.x = element_text(angle = 0, hjust = 0, vjust = 0)
         )
 }
 
@@ -35,10 +34,10 @@ plotDiffRelabund <- function(df, filter_soil_type) {
 
     gg <- df %>%
         filter(Soil_type == filter_soil_type) %>%
-        ggplot(aes(x = Fullscale, y = Downscaled, color = Bias)) +
+        ggplot(aes(x = Fullscale, y = Downscaled, color = pval_signif)) +
         geom_point(size = 4, alpha = 0.5, position = position_jitter()) +
         scale_color_manual(values = c("grey", "red", "blue")) +
-        geom_abline(size = 0.5) +
+        geom_abline(linewidth = 0.5) +
         articletheme +
         theme(
             legend.position = "bottom",
