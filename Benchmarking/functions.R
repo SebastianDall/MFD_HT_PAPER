fc76ed959224d30a3d9482e49bb93d7a6ad23119
Sebastian Dall <semoda@bio.aau.dk>
2022-11-28 11:30:10 +0100

    Benchmark: Changed ordination panel to include phylum vectors

--- Benchmarking/functions.R
+++ Benchmarking/functions.R
@@ -2,34 +2,8 @@ library(tidyverse)
 library(ggpubr)
 library(RColorBrewer)
 
-
-# Ordination function
-plotAmpliconOrdination <- function(df, ord_type = "CA", rel_ab_filter = 0.1) {
-    df %>%
-        amp_ordinate(
-            # sample_color_by = "Soil_type",
-            sample_shape_by = "Kit",
-            filter_species = rel_ab_filter,
-            type = ord_type,
-            transform = "hellinger",
-            distmeasure = "bray",
-            sample_colorframe = F,
-            species_plot = T
-        ) +
-        ggforce::geom_mark_ellipse(aes(fill = Soil_type, group = Soil_type), alpha = 0.5, color = "black") + # ggtitle("PCoA of microbial diversity", subtitle = bquote(atop(c("PERMANOVA", "Soil type:" ~R^2~ "= 0.54, p-value = 0.001", "Protocol:" ~R^2~ "= 0.02, p-value = 0.214")))) +
-        labs(
-            title = paste0(ord_type, " of microbial diversity"),
-            fill = "Sample type",
-            shape = "Extraction kit"
-        ) +
-        articletheme +
-        theme(
-            legend.position = "none",
-            axis.text.x = element_text(angle = 0)
-        )
-}
-
-plotAmpliconOrdinationSoil <- function(df, soil_filter, ord_type = "CA", rel_ab_filter = 0.1) {
+# Soil specific ordination
+plotAmpliconOrdinationSoil <- function(df, soil_filter, ord_type = "CA", envvector, rel_ab_filter = 0.1) {
     df %>%
         amp_subset_samples(Soil_type == soil_filter) %>%
         amp_ordinate(
@@ -38,9 +12,12 @@ plotAmpliconOrdinationSoil <- function(df, soil_filter, ord_type = "CA", rel_ab_
             constrain = "Kit",
             transform = "hellinger",
             distmeasure = "bray",
+            envfit_numeric = envvector,
             sample_colorframe = "Kit",
-            species_nlabels = 5,
-            species_label_taxonomy = "Phylum",
+            envfit_numeric_arrows_scale = 0.2,
+            envfit_arrowcolor = "#4bbbe1",
+            # species_nlabels = 5,
+            # species_label_taxonomy = "Phylum",
             species_plot = T
         ) +
         # ggforce::geom_mark_ellipse(aes(fill = Kit, group = Kit), alpha = 0.5, color = "black") +
@@ -55,6 +32,8 @@ plotAmpliconOrdinationSoil <- function(df, soil_filter, ord_type = "CA", rel_ab_
         )
 }
 
+# Add NA in red (not used anymore)
+
 addTableColor <- function(table, df, filter_col, column) {
     NA_vector <- df %>%
         filter(variable == filter_col, is.na(value))
@@ -65,16 +44,96 @@ addTableColor <- function(table, df, filter_col, column) {
 
 
 
-createGGMetricsPlotForCombined <- function(df, metric_filter, plot_legend = "none") {
-    colors <- c("grey", "darkgoldenrod2", "royalblue4", "seagreen", "salmon", "purple", "#0a6faa")
+# For custom ordination
 
-    ggobj <- df %>%
-        filter(metric == metric_filter) %>%
-        ggplot(aes(x = kit, y = value, color = fct_rev(soil_type_fct), group = interaction(soil_type_fct, kit))) +
-        geom_point(aes(shape = benchmark), size = 5, alpha = 0.5, position = position_dodge(width = 0.75)) +
-        scale_color_manual(values = colors) +
-        scale_fill_manual(values = colors) +
-        articletheme +
-        theme(legend.position = "bottom")
-    return(ggobj)
+
+generateAmpliconOrdinationData <- function(df, soil_filter, ord_type = "CA", envvector, rel_ab_filter = 0.1) {
+    df %>%
+        amp_subset_samples(Soil_type == soil_filter) %>%
+        amp_ordinate(
+            filter_species = 0.1,
+            type = ord_type,
+            constrain = "Kit",
+            transform = "hellinger",
+            distmeasure = "bray",
+            envfit_numeric = envvector,
+            sample_colorframe = "Kit",
+            # species_nlabels = 5,
+            # species_label_taxonomy = "Phylum",
+            detailed_output = T,
+            species_plot = T
+        )
+}
+
+plotMetadataOrdination <- function(df) {
+    plot <- df %>%
+        ggplot(aes(x = PC1, y = PC2, color = Kit, fill = Kit))
+
+    # samplecolorframe
+    plot <- plot + geom_polygon(aes(fill = Kit, group = Kit), alpha = 0.4, show.legend = FALSE)
+
+    # Sample points
+    plot <- plot + geom_point(size = 3)
+
+    return(plot)
+}
+
+addSpeciesPlot <- function(gg, df) {
+    plot <- gg +
+        geom_point(data = df, aes(x = PC1, y = PC2), inherit.aes = FALSE, size = 3, alpha = 0.6, color = "grey")
+
+    return(plot)
+}
+
+
+addEnvVector <- function(gg, df) {
+    scale_fac <- 0.5
+    pval_filter <- 0.005
+
+    envfit_df <- data.frame(
+        Name = rownames(df$vectors$arrows),
+        df$vectors$arrows * sqrt(df$vectors$r) * scale_fac,
+        pval = df$vectors$pvals
+    ) %>%
+        filter(pval < pval_filter) %>%
+        mutate(Name = str_replace(Name, "p\\_{2}", ""))
+
+    plot <- gg +
+        geom_segment(
+            data = envfit_df, aes(
+                x = 0,
+                xend = PC1,
+                y = 0,
+                yend = PC2
+            ), inherit.aes = FALSE,
+            arrow = arrow(length = unit(2, "mm")),
+            colour = "#e49c00",
+            linewidth = 1,
+            alpha = 0.7
+        ) +
+        geom_text_repel(
+            data = envfit_df,
+            aes(
+                x = PC1,
+                y = PC2,
+                label = Name
+            ), inherit.aes = FALSE,
+            colour = "black",
+            size = 3,
+            box.padding = 0.5,
+            fontface = "bold"
+        )
+
+    return(plot)
+}
+
+
+addLegendPlot <- function(plotlist) {
+    ggleg <- plotlist[[1]] + theme(legend.position = "right")
+
+    leg <- list(get_legend(ggleg))
+
+    plot_list2 <- append(plotlist, leg)
+
+    return(plot_list2)
 }
